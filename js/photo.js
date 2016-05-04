caa9fccdbe7250022df47cbbb5c9de58e256179b
xsnxsn <lzhouaj@connect.ust.hk>
2016-05-04 13:49:59 +0800

    Site updated: 2016-05-04 13:49:58

--- js/photo.js
+++ js/photo.js
@@ -18,8 +18,8 @@ define([], function () {
                 var html, li = "";
                 for (var i = begin; i < end && i < data.length; i++) {
                     li += '<li><div class="img-box">' +
-                        '<a class="img-bg" rel="example_group" href="https://github.com/lwzhang/blog/blob/master/photos/' + data[i] + '?raw=true"></a>' +
-                        '<img lazy-src="https://github.com/lwzhang/blog/blob/master/photos/' + data[i] + '?raw=true" />' +
+                        '<a class="img-bg" rel="example_group" href="https://github.com/xsnxsn/xsnxsn.github.io/photos/' + data[i] + '?raw=true"></a>' +
+                        '<img lazy-src="https://github.com/xsnxsn/xsnxsn.github.io/master/photos/' + data[i] + '?raw=true" />' +
                         '</li>';
                 }
 
